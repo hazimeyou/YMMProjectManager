38c39d00d859d79ca086252a84c5fbd3b75b07e7
hazimeyou <125953620+hazimeyou@users.noreply.github.com>
2026-05-07 02:48:35 +0900

    feat(ui): add progress popup with percent for diagnostics actions

--- YMMProjectManager/Presentation/Views/ExperimentalYmmTimelineHostWindow.xaml.cs
+++ YMMProjectManager/Presentation/Views/ExperimentalYmmTimelineHostWindow.xaml.cs
@@ -17,7 +17,7 @@ public partial class ExperimentalYmmTimelineHostWindow : Window
         Vm?.RedetectRuntime();
     }
 
-    private void OnRunProbeClick(object sender, RoutedEventArgs e)
+    private async void OnRunProbeClick(object sender, RoutedEventArgs e)
     {
         var options = new PureTimelineExperimentalOptions
         {
@@ -26,10 +26,10 @@ public partial class ExperimentalYmmTimelineHostWindow : Window
             AllowViewModelGenerationAttempt = false,
             OpenIsolatedHostWindow = false,
         };
-        Vm?.TryInitialize(options);
+        await RunWithProgressAsync("Reflection Probe を実行しています...", () => Vm?.TryInitialize(options));
     }
 
-    private void OnRunBindingDryRunClick(object sender, RoutedEventArgs e)
+    private async void OnRunBindingDryRunClick(object sender, RoutedEventArgs e)
     {
         var options = new PureTimelineExperimentalOptions
         {
@@ -38,16 +38,48 @@ public partial class ExperimentalYmmTimelineHostWindow : Window
             AllowViewModelGenerationAttempt = false,
             OpenIsolatedHostWindow = false,
         };
-        Vm?.TryInitialize(options);
+        await RunWithProgressAsync("Binding Dry-run を実行しています...", () => Vm?.TryInitialize(options));
     }
 
-    private void OnRunGenerationAttemptClick(object sender, RoutedEventArgs e)
+    private async void OnRunGenerationAttemptClick(object sender, RoutedEventArgs e)
     {
-        Vm?.TryRunGenerationAttempt();
+        await RunWithProgressAsync("生成試行(即破棄)を実行しています...", () => Vm?.TryRunGenerationAttempt());
     }
 
     private void OnSaveDiagnosticsClick(object sender, RoutedEventArgs e)
     {
         Vm?.SaveDiagnosticsSnapshot();
+    }
+
+    private async Task RunWithProgressAsync(string message, Action action)
+    {
+        var popup = new ProgressPopupWindow
+        {
+            Owner = this,
+            Message = message,
+            Percent = 5,
+        };
+
+        try
+        {
+            popup.Show();
+            popup.Percent = 15;
+            await Dispatcher.Yield(System.Windows.Threading.DispatcherPriority.Background);
+
+            popup.Percent = 35;
+            await Dispatcher.Yield(System.Windows.Threading.DispatcherPriority.Background);
+
+            action();
+
+            popup.Percent = 90;
+            await Dispatcher.Yield(System.Windows.Threading.DispatcherPriority.Background);
+
+            popup.Percent = 100;
+            await Task.Delay(80);
+        }
+        finally
+        {
+            popup.Close();
+        }
     }
 }
---
+++ YMMProjectManager/Presentation/Views/ProgressPopupWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace YMMProjectManager.Presentation.Views;
+
+public partial class ProgressPopupWindow : Window
+{
+    public static readonly DependencyProperty MessageProperty =
+        DependencyProperty.Register(
+            nameof(Message),
+            typeof(string),
+            typeof(ProgressPopupWindow),
+            new PropertyMetadata("処理を実行しています..."));
+
+    public static readonly DependencyProperty PercentProperty =
+        DependencyProperty.Register(
+            nameof(Percent),
+            typeof(double),
+            typeof(ProgressPopupWindow),
+            new PropertyMetadata(0d));
+
+    public string Message
+    {
+        get => (string)GetValue(MessageProperty);
+        set => SetValue(MessageProperty, value);
+    }
+
+    public double Percent
+    {
+        get => (double)GetValue(PercentProperty);
+        set => SetValue(PercentProperty, value);
+    }
+
+    public ProgressPopupWindow()
+    {
+        InitializeComponent();
+    }
+}
